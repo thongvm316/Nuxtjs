16afd10f938d403e0c1551b8b962f0d3cdd23228
thongvm316 <vominhthong1603@gmail.com>
2021-11-27 10:35:15 +0700

    first

---
+++ components/LargeCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="card">
+      <img
+        class="image"
+        :src="require(`@/assets/images/${card.image || 'fe1.jpg'}`)"
+        alt=""
+      />
+      <!-- :src for use params (card.image) -->
+      <h3 class="header">{{ card.title }}</h3>
+      <p class="snippet">
+        {{ card.snippet }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['card'],
+}
+</script>
+
+<style scoped>
+.card {
+  width: 31.5%;
+  height: 25rem;
+  border: none;
+  overflow: hidden;
+  padding: 0;
+  cursor: pointer;
+}
+.image {
+  height: 65%;
+  border-radius: 0.5rem;
+}
+.header {
+  font-size: 1.15rem;
+  margin-top: 0.4rem;
+}
+.snippet {
+  color: grey;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections } from '@/assets/data.js'
+
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+    }
+  },
+}
+</script>
